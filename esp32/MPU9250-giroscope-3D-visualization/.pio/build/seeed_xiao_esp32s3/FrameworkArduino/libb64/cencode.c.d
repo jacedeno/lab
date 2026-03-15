29e25d6c2d26c7b22772c4bdd0c0413861b90877
jacedeno <jacedeno@geekendzone.com>
2026-03-15 08:02:52 -0500

    Remove .pio build artifacts from tracking (now in .gitignore)

--- esp32/MPU9250-giroscope-3D-visualization/.pio/build/seeed_xiao_esp32s3/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/seeed_xiao_esp32s3/FrameworkArduino/libb64/cencode.c.o: \
- /home/geekendzone/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- /home/geekendzone/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
